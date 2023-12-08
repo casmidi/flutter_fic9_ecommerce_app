3a5883082d87e09a9a0d84e19702d36a23e6e776
casmidi <casmidiasli@yahoo.co.id>
2023-12-08 11:26:28 +0700

    commit for free error

--- lib/presentation/auth/bloc/register/register_bloc.dart
+++ lib/presentation/auth/bloc/register/register_bloc.dart
@@ -1,5 +1,6 @@
-import 'package:bloc/bloc.dart';
+// import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_fic9_ecommerce/data/datasources/auth_remote_datasource.dart';
 import 'package:flutter_fic9_ecommerce/data/models/request/register_request.dart';
 import 'package:flutter_fic9_ecommerce/data/models/responses/auth_response_model.dart';
@@ -12,7 +13,6 @@ part 'register_bloc.freezed.dart';
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
   RegisterBloc() : super(const _Initial()) {
     on<_Register>((event, emit)  async {
-      // TODO: implement event handler
       emit(const _Loading());
       final response = await AuthRemoteDatasource().register(event.data!);
       response.fold(
